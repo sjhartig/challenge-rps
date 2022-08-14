212e5572a1a530bd94af65dfd8fe93db772cf309
Steve Hartig <sjhartig@gmail.com>
2022-08-14 16:00:59 -0500

    Update basic layout including buttons
    Include header, footer, scenario and
    action sections with results.

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
+let uScore = 0;
+let cScore = 0;
+
 game();
 
 function getcompAnswer() {
@@ -13,22 +16,14 @@ function getcompAnswer() {
 }
 
 function compareResults(user, comp) {
-    if(user=== comp){
-        return("tie");
-    } else if (user === "rock" && comp === "paper") {
-        return(false);
-    } else if (user === "rock" && comp === "scissors") {
+    if (user === "rock" && comp === "scissors") {
         return(true);
-    } else if (user === "paper" && comp === "scissors") {
-        return(false);
     } else if (user === "paper" && comp === "rock") {
         return(true);
-    } else if (user === "scissors" && comp === "rock") {
-        return(false);
     } else if (user === "scissors" && comp === "paper") {
         return(true);
     } else {
-        return("bad input");
+        return false;
     }
 }
 
@@ -36,20 +31,12 @@ function game() {
     let uScore = 0;
     let cScore = 0;
 
-    while (uScore < 5 && cScore < 5) { 
-        let uAnswer = prompt("Lets play Rock, Paper, Scissors!").toLowerCase(); //a line break can be made with \n if needed
-        let cAnswer = getcompAnswer();
-
+    while (true) { 
         if (compareResults(uAnswer, cAnswer) === true) {
             uScore++;
-            alert("You win, " + uAnswer + " beats " + cAnswer + "!" + " Score: " + uScore + " to " + cScore);
-        } else if (compareResults(uAnswer, cAnswer) === false) {
-            cScore++;
-            alert("You lose, " + cAnswer + " beats " + uAnswer + "!" + " Score: " + uScore + " to " + cScore);
-        } else if (compareResults(uAnswer, cAnswer) === "tie") {
-            alert("It's a tie, " + uAnswer + " ties " + cAnswer + "!" + " Score: " + uScore + " to " + cScore);
         } else {
-            alert("You didn't type Rock, Paper or Scissors! Please try again.");
+            cScore++;
         }
     }
+
 }
